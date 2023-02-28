54708fdb573b07186e667612bc6c7ed287c692e0
meryvl <meryvalerolaguna@gmail.com>
2023-02-28 15:38:21 +0000

    styles commit

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -4,12 +4,15 @@ const SecondsCounter = (props) =>{
     return(
 <>
 
-<div class="bg-dark bg-gradient text-white text-center ">
+<div class="bg-dark bg-gradient text-white text-center">
     <div >
         <h1>React Counter</h1>
-    <div class="d-flex p-5">
-        <div>{props.counter1}</div>
-        <div>{props.counter}</div>
+    <div class=" d-flex justify-content-center fs-1 p-3 m-3 bg-secondary rounded">
+        <div class="text-primary  me-3 p-2  bg-light border border-4  rounded" >{props.counter4}</div>
+        <div class="text-warning  me-3 p-2 bg-light border border-4  rounded">{props.counter3}</div>
+        <div class="text-success   me-3 p-2 border bg-light border-4  rounded">{props.counter2}</div>
+        <div class="text-danger  me-3 p-2 border bg-light border-4 rounded">{props.counter1}</div>
+        <div class="text-primary   me-3 p-2 border bg-light border-4 rounded">{props.counter}</div>
         </div>
     <button onClick={props.clickOn}>Click On</button>
     <button>Click stop</button>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,21 +7,47 @@ const Home = () => {
 	
 	const [ counter, setCounter] = useState(0);
 	const [ counter1 , setCounter1 ] = useState(0)
-	const OnAdd = function(){
-		const newcounter = counter +1 ;
+	const [ counter2 , setCounter2 ] = useState(0)
+	const [ counter3 , setCounter3 ] = useState(0)
+	const [ counter4 , setCounter4 ] = useState(0)
+
+	
+	const containerOn = function(){
 		clearInterval(interval);
+		const newcounter = counter +1;
 		setCounter( newcounter);
+		if(counter == 9){
+			clearInterval(interval);
+			setCounter(0)
+			const newcounter1 = counter1 + 1
+			setCounter1( newcounter1)
+		}
+		if(counter1 == 9 ){
+			setCounter1(0)
+			const newcounter2 = counter2 + 1
+			setCounter2( newcounter2)
+		}
+		if(counter2 == 9 ){
+			setCounter2(0)
+			const newcounter3 = counter3 + 1
+			setCounter3( newcounter3)
+		}
+		if(counter3 == 9 ){
+			setCounter3(0)
+			const newcounter4 = counter4 + 1
+			setCounter4( newcounter4)
+		}
+		
 	}
-	if(counter == 10){
-		setCounter(0)
-		const newcounter1 = counter1 + 1
-		setCounter1( newcounter1)
-	}
-const interval=	setInterval(OnAdd,1000);
+	
+		const interval = setInterval(containerOn,1000);
+	
+	
+
 
 	return (
 		< >
-		<SecondsCounter counter={counter} counter1={counter1} clickon={OnAdd}/>
+		<SecondsCounter counter={counter} counter1={counter1} counter2={counter2} counter3={counter3} counter4={counter4}  clickOn={containerOn}/>
 		</>
 	);
 };
