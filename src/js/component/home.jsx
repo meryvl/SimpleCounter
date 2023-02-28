38d4832d085188db6500c1afb0763e8998f895e3
meryvl <meryvalerolaguna@gmail.com>
2023-02-28 17:28:29 +0000

    useEffect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 //include images into your bundle
 import SecondsCounter from "./SecondsCounter.jsx";
@@ -11,43 +11,41 @@ const Home = () => {
 	const [ counter3 , setCounter3 ] = useState(0)
 	const [ counter4 , setCounter4 ] = useState(0)
 
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+		  setCounter((counter) => counter + 1);
+		  
+		}, 1000);
 	
-	const containerOn = function(){
-		clearInterval(interval);
-		const newcounter = counter +1;
-		setCounter( newcounter);
-		if(counter == 9){
-			clearInterval(interval);
-			setCounter(0)
-			const newcounter1 = counter1 + 1
-			setCounter1( newcounter1)
-		}
-		if(counter1 == 9 ){
-			setCounter1(0)
-			const newcounter2 = counter2 + 1
-			setCounter2( newcounter2)
-		}
-		if(counter2 == 9 ){
-			setCounter2(0)
-			const newcounter3 = counter3 + 1
-			setCounter3( newcounter3)
-		}
-		if(counter3 == 9 ){
-			setCounter3(0)
-			const newcounter4 = counter4 + 1
-			setCounter4( newcounter4)
-		}
-		
+		return () => clearInterval(interval);
+	  }, []);
+	
+	  if(counter == 9){
+		setCounter(0)
+		const newcounter1 = counter1 + 1
+		setCounter1( newcounter1)
+	}	
+	if(counter1 == 9 ){
+		setCounter1(0)
+		const newcounter2 = counter2 + 1
+		setCounter2( newcounter2)
 	}
-	
-		const interval = setInterval(containerOn,1000);
-	
-	
+	if(counter2 == 9 ){
+		setCounter2(0)
+		const newcounter3 = counter3 + 1
+		setCounter3( newcounter3)
+	}
+	if(counter3 == 9 ){
+		setCounter3(0)
+		const newcounter4 = counter4 + 1
+		setCounter4( newcounter4)
+	}
 
 
 	return (
 		< >
-		<SecondsCounter counter={counter} counter1={counter1} counter2={counter2} counter3={counter3} counter4={counter4}  clickOn={containerOn}/>
+		<SecondsCounter counter={counter} counter1={counter1} counter2={counter2} counter3={counter3} counter4={counter4}  clickOn={useEffect}/>
 		</>
 	);
 };
