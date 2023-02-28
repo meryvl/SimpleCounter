af8a031ec39b369e1ea7620ff188bf66e8e2d9f7
meryvl <meryvalerolaguna@gmail.com>
2023-02-28 15:56:41 +0000

    style

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -4,18 +4,18 @@ const SecondsCounter = (props) =>{
     return(
 <>
 
-<div class="bg-dark bg-gradient text-white text-center">
+<div class="text-white text-center">
     <div >
         <h1>React Counter</h1>
-    <div class=" d-flex justify-content-center fs-1 p-3 m-3 bg-secondary rounded">
+    <div class=" d-flex justify-content-center fs-1 p-3 m-3 bg-danger rounded">
         <div class="text-primary  me-3 p-2  bg-light border border-4  rounded" >{props.counter4}</div>
         <div class="text-warning  me-3 p-2 bg-light border border-4  rounded">{props.counter3}</div>
         <div class="text-success   me-3 p-2 border bg-light border-4  rounded">{props.counter2}</div>
         <div class="text-danger  me-3 p-2 border bg-light border-4 rounded">{props.counter1}</div>
         <div class="text-primary   me-3 p-2 border bg-light border-4 rounded">{props.counter}</div>
         </div>
-    <button onClick={props.clickOn}>Click On</button>
-    <button>Click stop</button>
+    <button class="m-2 px-3 btn btn-info btn-outline-light rounded"  onClick={props.clickOn}>Click On</button>
+    <button class="m-2 px-3 btn btn-info btn-outline-light rounded">Click stop</button>
 
 
     
